586eb6de22be5dca6ed7baf94b709c56093a2d05
DESKTOP-MFM647T\hanaj <hanajin2@gmail.com>
2016-08-20 13:58:20 +0900

    daeho laucher image

--- app/src/main/java/com/example/hanaj/k_tour/MainActivity.java
+++ app/src/main/java/com/example/hanaj/k_tour/MainActivity.java
@@ -8,17 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
         setContentView(R.layout.launcher_layout);
-=======
 
-        setContentView(R.layout.activity_main);
-
-        System.out.println("123123123");
-
-        System.out.println("123123123");
-
-        System.out.println("123123123");
->>>>>>> 44c2c1a585b249a4f65cb099d9d47c857f27e31f
     }
 }
